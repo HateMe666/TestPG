b050230d893afeec3edd72d56014deb5e0bc1d2b
HateMe666 <nikita.kuzmich.11@gmail.com>
2022-06-07 17:35:10 +0300

    Update Test

--- MyTestSwiftPG.playground/Contents.swift
+++ MyTestSwiftPG.playground/Contents.swift
@@ -1,3 +1,150 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//
+//let km = 2.8
+//let timeInSeconds = 120
+//let value = Double(60) / Double(timeInSeconds) * km
+//
+//let km1 = 3.0
+//let time1 = 300
+//let value1 = Double(60) / Double(time1) * km1
+//
+//let km2 = 4.2
+//let time2 = 400
+//let value2 = Double(60) / Double(time2) * km2
+//
+//func calculateValue(km: Double, time: Int) {
+//    let value = Double(60) / Double(time) * km
+//print(value)
+//}
+//
+//calculateValue(km: 2.8, time: 120)
+//calculateValue(km: 3.0, time: 300)
+//calculateValue(km: 4.2, time: 400)
+//
+//print("Hello")
+//
+//
+//let number1 = 111
+//let number2 = 222
+//
+//let number3 = String(number1)
+//let number4 = String(number2)
+//let number5 = Int(number3 + number4)
+//
+//func calculateNumbers(number: Int, number2: Int) {
+//   print(Int(String(number) + String(number2))!)
+//}
+//calculateNumbers(number: 333, number2: 444)
+//
+//
+//print(9223372036854775807)
+//print(Int.max)
+//
+//
+// var a = 10
+// var b: Int = 20
+//var c = 30
+//var d = 40
+//var e = 50
+//
+//
+//let f = a
+//let g = b
+//let h = c
+//let i = d
+//
+//a = e
+//e = f
+//d = h
+//c = g
+//b = i
+
+import Foundation
+
+
+//let a: Float = 1.4
+//var b: Double = 2.5
+//b = 3.5
+
+//let a: Int = 18
+//let b: Float = 16.4
+//let c: Double = 5.7
+//
+//var sum: Float = (Float(a) + b + Float(c))
+//print(sum)
+//
+//var sum2: Int = (Int(Float(a) * b * Float(c)))
+//print(sum2)
+//
+//var sum3 = Double(b.truncatingRemainder(dividingBy: Float(c)))
+//print(sum3)
+//
+
+//var a: Int = 2
+//var b: Int = 3
+//print(Int(a + 4 * b) * Int(a - 3 * b) + a * a)
+//
+
+//var c = 1.75
+//var d = 3.25
+//print(2 * (c + 1) * 2 + 3 * (d + 1))
+//
+//let lenght = 11
+//
+//let s = lenght * lenght
+//let p = 2 * Int(3.14 * Double(lenght))
+//
+//print(s)
+//print(p)
+//print(Double(s) * Double(p))
+
+
+//var text: String = "Hello"
+//let char: Character = "F"
+//var a: Int = 10
+//var b: Int = 20
+//
+//let total = text + String(char) + String(a + b)
+//print(total)
+//
+
+
+//var day = 10
+//var month = "February"
+//var year = 2022
+//print(year, month, day)
+//
+//
+//
+//
+//var a = true
+//var b = false
+//
+//a && b
+//a || b
+//
+//
+//let name = "Nikita"
+//var weight: Float = 72.3
+//var height: Int = 188
+//
+//var imt = weight / (Float(height) / Float(100) * (Float(height) / Float(100)))
+//print(imt)
+//
+//print("My name is \(name). My IMT is \(imt)")
+//
+//
+//
+//
+//
+
+
+let a: Double = 1.4
+let b: Double = 3.2
+
+let c: Float = (Float(a + b) / 2)
+print(c)
+
+
+
